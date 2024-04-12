ae2bfe33765e4a26d9b5dfd5af59abe763bf3861
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-12 10:00:54 +0200

    start

---
+++ src/main/java/bkendpjw5/pjw5/PjW5Application.java
@@ -0,0 +1,13 @@
+package bkendpjw5.pjw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjW5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjW5Application.class, args);
+	}
+
+}
---
+++ src/test/java/bkendpjw5/pjw5/PjW5ApplicationTests.java
@@ -0,0 +1,13 @@
+package bkendpjw5.pjw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjW5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
